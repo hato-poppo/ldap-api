61b10243f327611643fcf5e2e6e8c89d1fbe9305
hato-poppo <ysng1224@gmail.com>
2021-08-20 23:29:20 +0900

    [Change] Code refactoring.

--- application/service/ldap_application.go
+++ application/service/ldap_application.go
@@ -3,12 +3,11 @@ package application
 import (
     "fmt"
     repository "ldap/domain/service"
-    model "ldap/domain/model" // ここでmodelを参照して良いかどうか未確認
+    model "ldap/domain/model"
 )
 
 // ここでコントローラー的な役割を担う
-// 適切な名前が浮かばなかったから適当な名前
-func Controller(server repository.LdapRepository) model.LdapResult {
+func LdapUseCase(server repository.LdapRepository) model.LdapResult {
     ldapServer := model.Ldap{Server: server}
     return ldapServer.Search()
 }
