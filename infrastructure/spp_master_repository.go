2c8a7b9c48c40924690772348deeb6188e07e5e2
hato-poppo <ysng1224@gmail.com>
2021-08-22 21:17:55 +0900

    [Add] Comment.

--- infrastructure/spp_master_repository.go
+++ infrastructure/spp_master_repository.go
@@ -29,11 +29,14 @@ func NewSppMaster() *sppMaster {
 
 
 func (s sppMaster) Connect() bool {
+    // ここで接続情報を参照する
     fmt.Println("#infrastructure: LDAP server connected.")
     return true
 }
 
 func (s sppMaster) Search() (string, string, string) {
+    // Connectは外部で宣言するよりも内部から読んだ方が良いが、connectにするとうまく動作しないので一旦諦めている
     fmt.Println("#infrastructure: LDAP server searched.")
+    // 接続に失敗したら例外投げたい
     return "UserId", "UserName", "Email"
 }
